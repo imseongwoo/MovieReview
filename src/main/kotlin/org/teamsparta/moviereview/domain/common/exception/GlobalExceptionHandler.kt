398f8ecd7f721c6bc4c238881b2b7a06bf225413
임성우 <seongwoorang@naver.com>
2024-07-02 19:13:18 +0900

    init

---
+++ src/main/kotlin/org/teamsparta/moviereview/domain/common/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,7 @@
+package org.teamsparta.moviereview.domain.common.exception
+
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+}
---
+++ src/main/kotlin/org/teamsparta/moviereview/domain/common/exception/dto/ErrorResponse.kt
@@ -0,0 +1,5 @@
+package org.teamsparta.moviereview.domain.common.exception.dto
+
+data class ErrorResponse (
+    val message: String?,
+)
