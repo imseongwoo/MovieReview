dc5eb118ba08016741b59d421831d12f832824a0
dongheon0827 <dongheon827@naver.com>
2024-07-03 20:12:08 +0900

    :recycle: #13 - Repository annotation 상속

--- src/main/kotlin/org/teamsparta/moviereview/domain/comment/repository/v1/CommentRepository.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/comment/repository/v1/CommentRepository.kt
@@ -1,7 +1,9 @@
 package org.teamsparta.moviereview.domain.comment.repository.v1
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import org.teamsparta.moviereview.domain.comment.model.Comment
 
+@Repository
 interface CommentRepository : JpaRepository<Comment, Long> {
 }
