7c4ed1363fe7308ba60f6a598208e67c0726b888
임성우 <seongwoorang@naver.com>
2024-07-03 16:08:08 +0900

    :sparkles: #1 - 예외처리 추가

--- src/main/kotlin/org/teamsparta/moviereview/domain/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/common/exception/GlobalExceptionHandler.kt
@@ -1,7 +1,25 @@
 package org.teamsparta.moviereview.domain.common.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.teamsparta.moviereview.domain.common.InvalidCredentialException
+import org.teamsparta.moviereview.domain.common.exception.dto.ErrorResponse
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(InvalidCredentialException::class)
+    fun handleInvalidCredentialException(e: InvalidCredentialException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity
+            .status(HttpStatus.UNAUTHORIZED)
+            .body(ErrorResponse(e.message))
+    }
 }
