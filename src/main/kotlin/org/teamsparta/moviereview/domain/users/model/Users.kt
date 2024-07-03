e4d70cb690b9cccf8494ea121911b3e134085935
임성우 <seongwoorang@naver.com>
2024-07-03 16:21:53 +0900

    :sparkles: #1 - entity 추가

--- src/main/kotlin/org/teamsparta/moviereview/domain/users/model/Users.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/users/model/Users.kt
@@ -1,4 +1,32 @@
 package org.teamsparta.moviereview.domain.users.model
 
-class Users {
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "app_user")
+class Users(
+    @Column(name = "email", nullable = false)
+    val email: String,
+
+    @Column(name = "password")
+    var password: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    val role: UserRole = UserRole.USER,
+
+    @Column(name = "nickname", nullable = true)
+    val nickname: String,
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "deleted_at", nullable = true)
+    var deletedAt: LocalDateTime = LocalDateTime.now(),
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
 }
