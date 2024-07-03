032d2e1002964458257b54aa0a6f8af9fc0646d7
임성우 <seongwoorang@naver.com>
2024-07-03 17:14:48 +0900

    :sparkles: #3 - 로그인 로직 추가

--- src/main/kotlin/org/teamsparta/moviereview/domain/users/controller/v1/UsersController.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/users/controller/v1/UsersController.kt
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.teamsparta.moviereview.domain.users.dto.LoginRequest
+import org.teamsparta.moviereview.domain.users.dto.LoginResponse
 import org.teamsparta.moviereview.domain.users.dto.SignUpRequest
 import org.teamsparta.moviereview.domain.users.dto.UserDto
 import org.teamsparta.moviereview.domain.users.service.v1.UserService
@@ -24,5 +26,12 @@ class UsersController(
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(userService.signUp(signUpRequest))
+    }
+
+    @PostMapping("/login")
+    fun signIn(@RequestBody loginRequest: LoginRequest): ResponseEntity<LoginResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(userService.signIn(loginRequest))
     }
 }
--- src/main/kotlin/org/teamsparta/moviereview/domain/users/service/v1/UserService.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/users/service/v1/UserService.kt
@@ -1,8 +1,11 @@
 package org.teamsparta.moviereview.domain.users.service.v1
 
+import org.teamsparta.moviereview.domain.users.dto.LoginRequest
+import org.teamsparta.moviereview.domain.users.dto.LoginResponse
 import org.teamsparta.moviereview.domain.users.dto.SignUpRequest
 import org.teamsparta.moviereview.domain.users.dto.UserDto
 
 interface UserService {
     fun signUp(request: SignUpRequest): UserDto
+    fun signIn(loginRequest: LoginRequest): LoginResponse
 }
