7fe0f9f16e3ad1adff335a1c0979020d0b19df64
dongheon0827 <dongheon827@naver.com>
2024-07-03 15:03:09 +0900

    :sparkles: #6 - comment CUD API ready

--- src/main/kotlin/org/teamsparta/moviereview/domain/comment/service/v1/CommentServiceImpl.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/comment/service/v1/CommentServiceImpl.kt
@@ -1,4 +1,21 @@
 package org.teamsparta.moviereview.domain.comment.service.v1
 
-class CommentServiceImpl {
+import org.springframework.stereotype.Service
+import org.teamsparta.moviereview.domain.comment.dto.CommentCreateRequest
+import org.teamsparta.moviereview.domain.comment.dto.CommentResponse
+import org.teamsparta.moviereview.domain.comment.dto.CommentUpdateRequest
+
+@Service
+class CommentServiceImpl : CommentService {
+    override fun createComment(request: CommentCreateRequest): CommentResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateComment(commentId: Long, request: CommentUpdateRequest): CommentResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteComment(commentId: Long) {
+        TODO("Not yet implemented")
+    }
 }
