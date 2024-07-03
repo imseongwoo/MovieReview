101c39e94f9dc81eca297a6e2206591d4b9d4293
dongheon0827 <162969955+dongheon0827@users.noreply.github.com>
2024-07-03 16:34:32 +0900

    ✨ #6 - feat - comment entity dto (#21)
    * Update issue templates
    
    * Update issue templates
    
    * Create pull_request_template.md
    
    * :sparkles: #6 - comment dto
    
    * :sparkles: #6 - comment entity model
    
    * :sparkles: #6 - comment repository jpa 상속
    
    * :sparkles: #6 - comment CUD API ready
    
    * :sparkles: #14 - Keyword Entity 생성 (#20)
    
    * :sparkles: #14 - Keyword Entity 생성
    
    * :sparkles: #14 - companion object 객체 생성
    
    * Update issue templates
    
    * Update issue templates
    
    * Create pull_request_template.md
    
    * :sparkles: #6 - comment dto
    
    * :sparkles: #6 - comment entity model
    
    * :sparkles: #6 - comment repository jpa 상속
    
    * :sparkles: #6 - comment CUD API ready
    
    * :recycle:  #6 - Entity toResponse dto에서 처리
    
    * :recycle: #6 - ~~At BaseEntity 상속받아 사용
    
    ---------
    
    Co-authored-by: 임성우 <seongwoorang@naver.com>
    Co-authored-by: JinkownHong <161419351+JinkownHong@users.noreply.github.com>

---
+++ .github/ISSUE_TEMPLATE/issue-template.md
@@ -0,0 +1,14 @@
+---
+name: issue template
+about: 이슈 템플릿입니다.
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+## 내용 소개
+
+### 구현 내용
+- [ ] 구현내용1
+- [ ] 구현내용2
--- src/main/kotlin/org/teamsparta/moviereview/domain/comment/model/Comment.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/comment/model/Comment.kt
@@ -1,4 +1,37 @@
 package org.teamsparta.moviereview.domain.comment.model
 
-class Comment {
+import jakarta.persistence.*
+import org.teamsparta.moviereview.domain.comment.dto.CommentResponse
+import org.teamsparta.moviereview.domain.comment.dto.CommentUpdateRequest
+import org.teamsparta.moviereview.domain.common.time.BaseEntity
+import org.teamsparta.moviereview.domain.post.model.Post
+import org.teamsparta.moviereview.domain.users.model.Users
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "comment")
+class Comment(
+    @Column
+    var content: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    var post: Post,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    var user: Users,
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    fun updateContent(commentUpdateRequest: CommentUpdateRequest) {
+        this.content = commentUpdateRequest.content
+        this.updatedAt = LocalDateTime.now()
+    }
+
+    fun delete() {
+        this.deletedAt = LocalDateTime.now()
+    }
 }
