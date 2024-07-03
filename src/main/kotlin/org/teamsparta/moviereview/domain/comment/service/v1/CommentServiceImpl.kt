ceeb7f855d87e63ce32fd1d9ca656bbe16a4b97d
dongheon0827 <162969955+dongheon0827@users.noreply.github.com>
2024-07-03 21:04:46 +0900

    ✨ #13 - 댓글 CRUD 기능 (#31)
    * Update issue templates
    
    * Update issue templates
    
    * :recycle: #13 - Repository annotation 상속
    
    * :sparkles: #13 - Comment Service Layer 작성
    
    * :sparkles: #13 - Comment Controller 작성
    
    ---------
    
    Co-authored-by: 임성우 <seongwoorang@naver.com>

--- src/main/kotlin/org/teamsparta/moviereview/domain/comment/service/v1/CommentServiceImpl.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/comment/service/v1/CommentServiceImpl.kt
@@ -1,21 +1,51 @@
 package org.teamsparta.moviereview.domain.comment.service.v1
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.teamsparta.moviereview.domain.comment.dto.CommentCreateRequest
 import org.teamsparta.moviereview.domain.comment.dto.CommentResponse
 import org.teamsparta.moviereview.domain.comment.dto.CommentUpdateRequest
+import org.teamsparta.moviereview.domain.comment.model.Comment
+import org.teamsparta.moviereview.domain.comment.repository.v1.CommentRepository
+import org.teamsparta.moviereview.domain.common.exception.ModelNotFoundException
+import org.teamsparta.moviereview.domain.post.model.Post
+import org.teamsparta.moviereview.domain.post.repository.v1.PostRepository
+import org.teamsparta.moviereview.domain.users.model.Users
+import org.teamsparta.moviereview.domain.users.repository.v1.UserRepository
 
 @Service
-class CommentServiceImpl : CommentService {
-    override fun createComment(request: CommentCreateRequest): CommentResponse {
-        TODO("Not yet implemented")
+class CommentServiceImpl(
+    private val postRepository: PostRepository,
+    private val userRepository: UserRepository,
+    private val commentRepository: CommentRepository
+) : CommentService {
+    override fun createComment(postId: Long, request: CommentCreateRequest, email: String): CommentResponse {
+        val post: Post = postRepository.findByIdOrNull(postId) ?: throw ModelNotFoundException("post", postId)
+        val user: Users = userRepository.findByEmail(email) ?: throw IllegalStateException()
+
+        val comment: Comment = Comment(
+            content = request.content,
+            post = post,
+            user = user
+        )
+        commentRepository.save(comment)
+        return CommentResponse.fromEntity(comment)
     }
 
     override fun updateComment(commentId: Long, request: CommentUpdateRequest): CommentResponse {
-        TODO("Not yet implemented")
+        val comment: Comment =
+            commentRepository.findByIdOrNull(commentId) ?: throw ModelNotFoundException("comment", commentId)
+
+        comment.updateContent(request)
+        commentRepository.save(comment)
+        return CommentResponse.fromEntity(comment)
     }
 
     override fun deleteComment(commentId: Long) {
-        TODO("Not yet implemented")
+        val comment: Comment =
+            commentRepository.findByIdOrNull(commentId) ?: throw ModelNotFoundException("comment", commentId)
+
+        comment.delete()
+        commentRepository.save(comment)
     }
 }
