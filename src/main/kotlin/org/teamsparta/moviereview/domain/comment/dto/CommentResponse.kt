ab4bbe4d0917fcaffc097020665159c430db1d30
dongheon0827 <dongheon827@naver.com>
2024-07-03 15:38:45 +0900

    :recycle:  #6 - Entity toResponse dto에서 처리

--- src/main/kotlin/org/teamsparta/moviereview/domain/comment/dto/CommentResponse.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/comment/dto/CommentResponse.kt
@@ -1,5 +1,6 @@
 package org.teamsparta.moviereview.domain.comment.dto
 
+import org.teamsparta.moviereview.domain.comment.model.Comment
 import java.time.LocalDateTime
 
 data class CommentResponse(
@@ -7,4 +8,15 @@ data class CommentResponse(
     val nickname: String,
     val content: String,
     val createdAt: LocalDateTime,
-)
+){
+    companion object {
+        fun fromEntity(comment: Comment): CommentResponse {
+            return CommentResponse(
+                id = comment.id!!,
+                nickname = comment.user.nickname,
+                content = comment.content,
+                createdAt = comment.createdAt,
+            )
+        }
+    }
+}
