1a21931105d8dcb4a171bdd388e9275b30b210b7
임성우 <seongwoorang@naver.com>
2024-07-03 16:21:08 +0900

    :sparkles: #1 - 유효성 검증 에러 handler 추가

--- src/main/kotlin/org/teamsparta/moviereview/domain/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/common/exception/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package org.teamsparta.moviereview.domain.common.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.teamsparta.moviereview.domain.common.InvalidCredentialException
@@ -21,5 +22,21 @@ class GlobalExceptionHandler {
         return ResponseEntity
             .status(HttpStatus.UNAUTHORIZED)
             .body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<MutableMap<String, Any>> {
+        val body: MutableMap<String, Any> = mutableMapOf("statusCode" to "400")
+        val errors: MutableMap<String, String> = mutableMapOf()
+
+        e.bindingResult.fieldErrors.forEach { fieldError ->
+            errors[fieldError.field] = fieldError.defaultMessage ?: ""
+        }
+
+        body["errors"] = errors
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(body)
     }
 }
