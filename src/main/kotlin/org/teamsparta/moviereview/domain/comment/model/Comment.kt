51b468de4b506e2120610731eb6033222f1ba00f
dongheon0827 <dongheon827@naver.com>
2024-07-03 15:39:18 +0900

    :recycle: #6 - ~~At BaseEntity 상속받아 사용

--- src/main/kotlin/org/teamsparta/moviereview/domain/comment/model/Comment.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/comment/model/Comment.kt
@@ -3,6 +3,7 @@ package org.teamsparta.moviereview.domain.comment.model
 import jakarta.persistence.*
 import org.teamsparta.moviereview.domain.comment.dto.CommentResponse
 import org.teamsparta.moviereview.domain.comment.dto.CommentUpdateRequest
+import org.teamsparta.moviereview.domain.common.time.BaseEntity
 import org.teamsparta.moviereview.domain.post.model.Post
 import org.teamsparta.moviereview.domain.users.model.Users
 import java.time.LocalDateTime
@@ -13,15 +14,6 @@ class Comment(
     @Column
     var content: String,
 
-    @Column(name = "created_at", nullable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now(),
-
-    @Column(name = "updated_at")
-    var updatedAt: LocalDateTime,
-
-    @Column(name = "deleted_at")
-    var deletedAt: LocalDateTime,
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id")
     var post: Post,
@@ -29,19 +21,10 @@ class Comment(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     var user: Users,
-) {
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
-
-    fun toResponse(): CommentResponse {
-        return CommentResponse(
-            id = id!!,
-            content = content,
-            createdAt = createdAt,
-            nickname = user.nickname, // 닉네임 주입 필요
-        )
-    }
 
     fun updateContent(commentUpdateRequest: CommentUpdateRequest) {
         this.content = commentUpdateRequest.content
