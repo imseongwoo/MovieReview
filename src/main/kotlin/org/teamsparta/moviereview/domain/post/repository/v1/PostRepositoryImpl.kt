0ab597a337b900d0cdbd8a7c74c49022f97c701c
dongheon0827 <dongheon827@naver.com>
2024-07-04 20:09:29 +0900

    :sparkles: #48 - searchPost에 사용할 queryDSL 활용 repository :recycle: #48 - 바뀐 변수명에 맞게 함수명 변경

--- src/main/kotlin/org/teamsparta/moviereview/domain/post/repository/v1/PostRepositoryImpl.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/post/repository/v1/PostRepositoryImpl.kt
@@ -1,33 +1,46 @@
 package org.teamsparta.moviereview.domain.post.repository.v1
 
 import com.querydsl.core.BooleanBuilder
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 import org.teamsparta.moviereview.domain.comment.model.QComment
-import org.teamsparta.moviereview.domain.post.model.Category
-import org.teamsparta.moviereview.domain.post.model.Post
+import org.teamsparta.moviereview.domain.post.dto.PostResponseWithThumbsUpAndComments
 import org.teamsparta.moviereview.domain.post.model.QPost
+import org.teamsparta.moviereview.domain.post.model.search.Keyword
 import org.teamsparta.moviereview.domain.post.model.thumbsup.QThumbsUp
+import org.teamsparta.moviereview.domain.post.repository.v1.search.KeywordRepository
 import org.teamsparta.moviereview.domain.users.model.QUsers
 import org.teamsparta.moviereview.infra.querydsl.QueryDslSupport
+import java.time.LocalDateTime
 
 @Repository
-class PostRepositoryImpl : CustomPostRepository, QueryDslSupport() {
+class PostRepositoryImpl(private val keywordRepository: KeywordRepository) : CustomPostRepository, QueryDslSupport() {
 
     private val post = QPost.post
     private val comment = QComment.comment
     private val user = QUsers.users
     private val thumbsUp = QThumbsUp.thumbsUp
 
-    override fun findAllByPageableAndCategory(pageable: Pageable, category: String?): Triple<Long, List<Post>, List<Long>> {
+    override fun findAllByPageableAndKeyword(
+        pageable: Pageable,
+        keyword: String?
+    ): Page<PostResponseWithThumbsUpAndComments> {
 
         val whereClause = BooleanBuilder()
 
-        category?.let { whereClause.and(post.category.eq(Category.fromString(it))) }
+        keyword?.let {
+            whereClause.and(
+                post.title.containsIgnoreCase(it)
+                    .or(post.content.containsIgnoreCase(it))
+            )
+            saveSearchKeyword(it)
+        }
 
-        val totalCount = queryFactory.select(post.count()).from(post)
+        val totalCount = queryFactory.selectFrom(post)
             .where(whereClause)
-            .fetchOne() ?: 0L
+            .fetchCount()
 
         val posts = queryFactory.selectFrom(post)
             .leftJoin(post.user, user).fetchJoin()
@@ -37,17 +50,35 @@ class PostRepositoryImpl : CustomPostRepository, QueryDslSupport() {
             .limit(pageable.pageSize.toLong())
             .fetch()
 
-        val thumbsUpCounts = queryFactory.select(thumbsUp.count())
-            .from(post).leftJoin(thumbsUp)
-            .on(thumbsUp.postId.eq(post.id))
-            .where(whereClause)
-            .groupBy(post.id)
-            .orderBy(post.createdAt.desc())
-            .offset(pageable.offset)
-            .limit(pageable.pageSize.toLong())
+        val postIds = posts.map { it.id }
+
+        val thumbsUpCounts = queryFactory
+            .select(thumbsUp.postId, thumbsUp.count())
+            .from(thumbsUp)
+            .where(thumbsUp.postId.`in`(postIds))
+            .groupBy(thumbsUp.postId)
             .fetch()
+            .associate { it[thumbsUp.postId] to it[thumbsUp.count()] }
 
-        return Triple(totalCount, posts, thumbsUpCounts)
+        val commentsMap = queryFactory
+            .select(comment)
+            .where(comment.post.id.`in`(postIds))
+            .fetch()
+            .groupBy { it.post.id }
 
+        val postResponseWithThumbsUpAndComments = posts.map { post ->
+            PostResponseWithThumbsUpAndComments(
+                post = post,
+                thumbsUpCount = thumbsUpCounts[post.id] ?: -1L,
+                comments = commentsMap[post.id] ?: emptyList()
+            )
+        }
+
+        return PageImpl(postResponseWithThumbsUpAndComments, pageable, totalCount)
+    }
+
+    fun saveSearchKeyword(keyword: String) {
+        val keyword = Keyword(keyword = keyword, createdAt = LocalDateTime.now())
+        keywordRepository.save(keyword)
     }
 }
