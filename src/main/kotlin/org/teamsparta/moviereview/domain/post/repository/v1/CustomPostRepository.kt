552685e99f8c4b11a809c7b97046773baaf7928a
dongheon0827 <dongheon827@naver.com>
2024-07-04 20:12:46 +0900

    :recycle: #48 - Keyword table에서 바꾼 변수명에 맞게 변경 & paging 반환

--- src/main/kotlin/org/teamsparta/moviereview/domain/post/repository/v1/CustomPostRepository.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/post/repository/v1/CustomPostRepository.kt
@@ -1,8 +1,9 @@
 package org.teamsparta.moviereview.domain.post.repository.v1
 
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.teamsparta.moviereview.domain.post.model.Post
+import org.teamsparta.moviereview.domain.post.dto.PostResponseWithThumbsUpAndComments
 
 interface CustomPostRepository {
-    fun findAllByPageableAndCategory(pageable: Pageable, category: String?): Triple<Long, List<Post>, List<Long>>
+    fun findAllByPageableAndKeyword(pageable: Pageable, keyword: String?): Page<PostResponseWithThumbsUpAndComments>
 }
