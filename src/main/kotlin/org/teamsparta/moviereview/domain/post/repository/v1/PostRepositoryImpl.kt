54f28bc58a3ebd39e502d6c1f36165da49b49be9
dongheon0827 <dongheon827@naver.com>
2024-07-05 10:00:39 +0900

    :sparkles: #48 - searchPost Repository feature

--- src/main/kotlin/org/teamsparta/moviereview/domain/post/repository/v1/PostRepositoryImpl.kt
+++ src/main/kotlin/org/teamsparta/moviereview/domain/post/repository/v1/PostRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package org.teamsparta.moviereview.domain.post.repository.v1
 
 import com.querydsl.core.BooleanBuilder
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 import org.teamsparta.moviereview.domain.comment.model.QComment
+import org.teamsparta.moviereview.domain.post.dto.PostResponse
 import org.teamsparta.moviereview.domain.post.model.Category
 import org.teamsparta.moviereview.domain.post.model.Post
 import org.teamsparta.moviereview.domain.post.model.QPost
@@ -19,7 +22,10 @@ class PostRepositoryImpl : CustomPostRepository, QueryDslSupport() {
     private val user = QUsers.users
     private val thumbsUp = QThumbsUp.thumbsUp
 
-    override fun findAllByPageableAndCategory(pageable: Pageable, category: String?): Triple<Long, List<Post>, List<Long>> {
+    override fun findAllByPageableAndCategory(
+        pageable: Pageable,
+        category: String?
+    ): Triple<Long, List<Post>, List<Long>> {
 
         val whereClause = BooleanBuilder()
 
@@ -49,5 +55,55 @@ class PostRepositoryImpl : CustomPostRepository, QueryDslSupport() {
 
         return Triple(totalCount, posts, thumbsUpCounts)
 
+    }
+
+    override fun searchPostByPageableAndKeyword(
+        pageable: Pageable,
+        keyword: String?
+    ): Page<PostResponse> {
+        val whereClause = BooleanBuilder()
+
+        keyword?.let {
+            whereClause.and(
+                post.title.containsIgnoreCase(it)
+                    .or(post.content.containsIgnoreCase(it))
+            )
+        }
+
+        val totalCount = queryFactory.selectFrom(post)
+            .where(whereClause)
+            .fetchCount()
+
+        val posts = queryFactory.selectFrom(post)
+            .leftJoin(post.user, user).fetchJoin()
+            .where(whereClause)
+            .orderBy(post.createdAt.desc())
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .fetch()
+
+        val postIds = posts.map { it.id }
+
+        val thumbsUpCounts = queryFactory
+            .select(thumbsUp.postId, thumbsUp.count())
+            .from(thumbsUp)
+            .where(thumbsUp.postId.`in`(postIds))
+            .groupBy(thumbsUp.postId)
+            .fetch()
+            .associate { it[thumbsUp.postId] to it[thumbsUp.count()] }
+
+        val postResponse = posts.map { post ->
+            PostResponse(
+                id = post.id!!,
+                title = post.title,
+                nickname = post.user.nickname,
+                content = post.content,
+                category = post.category.toString(),
+                thumbUpCount = thumbsUpCounts[post.id] ?: 0L,
+                createdAt = post.createdAt,
+                updatedAt = post.updatedAt
+            )
+        }
+        return PageImpl(postResponse, pageable, totalCount)
     }
 }
